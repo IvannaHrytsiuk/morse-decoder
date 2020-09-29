cc27618982bbddcbac0e2fd4cd620a00a9192a37
IvannaHrytsiuk <izink0.0@gmail.com>
2020-09-29 18:18:43 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,32 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arr10 = [];
+    let res = '';
+    let dot = '.';
+    let hyphen = '-';
+    for(let i=0; i<expr.length; i+=10){
+        arr10.push(expr.slice(i, i+10));
+    } 
+    for( let i=0; i<arr10.length; i++){
+        if(arr10[i] == '**********'){
+            res += ' ';
+        } else{
+            let str = '';
+            for (let k=0; k< arr10[i].length; k+= 2) {
+                if(arr10[i].substr(k, 2) == '00'){
+                    str += '';
+                } else if(arr10[i].substr(k, 2) == '10'){
+                    str += dot;
+                } else{
+                    str += hyphen
+                }
+            }
+            res += MORSE_TABLE[str];
+        }
+    }
+    console.log(res)
+    return res;
 }
 
 module.exports = {
